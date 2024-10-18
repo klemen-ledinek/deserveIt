789aaa507d50046d75fd2e6df68b38f51a4839f4
Klemen Ledinek <klemen.ledinek@gmail.com>
2024-10-18 20:45:27 +0200

    Removing comments

--- src/main.py
+++ src/main.py
@@ -14,7 +14,6 @@ def home():
     tasks = c.fetchall()
     conn.close()
     return render_template('index.html', tasks=tasks)
-#    return render_template("index.html")
 
 
 @app.route('/perform-action', methods=['POST'])
@@ -37,23 +36,4 @@ def perform_action():
     c.execute('UPDATE whish_list SET time = ? WHERE id = ? ', (result_time_str, id));
     conn.commit()
     conn.close()
-    # Fetch the current time from the database
-    # cursor.execute('SELECT id, event_time FROM events')
-    # events = cursor.fetchall()
-
-    # for event in events:
-    #     event_id, event_time = event
-    #     # Parse the event_time string to a datetime object
-    #     event_time_dt = datetime.strptime(event_time, '%Y-%m-%d %H:%M:%S')
-
-    #     # Decrement by 10 minutes
-    #     new_event_time = event_time_dt - timedelta(minutes=10)
-
-    #     # Update the database with the new time
-    #     cursor.execute('''
-    #         UPDATE events SET event_time = ? WHERE id = ?
-    #     ''', (new_event_time.strftime('%Y-%m-%d %H:%M:%S'), event_id))
-
-    # conn.commit()
-    # conn.close()
     return redirect(url_for('home'))  # Redirect back to the main page
